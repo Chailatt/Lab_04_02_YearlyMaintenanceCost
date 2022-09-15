ea85a4f401413643431efe992eb525d3adfc7eb0
Chevez Robertson <chai@dhcp-172-30-226-122.guest.uc.edu>
2022-09-15 15:16:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       double Winterprice = 44.50;
+       double springprice = 36.50;
+       double summerprice = 50.00;
+       double fallprice = 28.50;
+
+      double totalcost = Winterprice + springprice + summerprice + fallprice;
+
+      System.out.println("Your total cost for all 4 seasons is " + "$" + totalcost );
+
+    }
+}
